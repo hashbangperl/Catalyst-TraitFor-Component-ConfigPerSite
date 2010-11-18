767187ecfa6c28d32c1240a0ff5d7ed7b51fcad1
Aaron J Trevena <aaron.trevena@gmail.com>
2010-11-18 11:38:08 +0000

    use trait, move config into file

--- t/lib/TestBlogApp/Model/DB.pm
+++ t/lib/TestBlogApp/Model/DB.pm
@@ -1,15 +1,10 @@
 package TestBlogApp::Model::DB;
+use strict;
 
-use base qw/ Catalyst::Model::DBIC::Schema /;
+use Moose;
 
+extends 'Catalyst::Model::DBIC::Schema'; 
+with qw(Catalyst::TraitFor::Model::DBIC::ConfigPerSite)
 
-__PACKAGE__->config(
-                 schema_class => 'TestBlogApp::Schema',
-                 connect_info => {
-                                   dsn => "dbi:SQLite:dbname=t/test.db",
-                                   user => "username",
-                                   password => "password",
-                                 }
-             );
 
 1;
