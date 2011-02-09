71b8f90cd79ac54039d01afd7d6fba791f63451d
Aaron J Trevena <aaron.trevena@gmail.com>
2011-02-09 21:55:10 +0000

    added missing files, new release

--- lib/Catalyst/TraitFor/Component/ConfigPerSite.pm
+++ lib/Catalyst/TraitFor/Component/ConfigPerSite.pm
@@ -61,11 +61,13 @@ default_view TT
 
 =head1 VERSION
 
-0.02
+0.03
+
+Dedicated to Smylers who won the 2009 cryptic christmas crossword competition
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.0';
 
 use Moose::Role;
 use MRO::Compat;
