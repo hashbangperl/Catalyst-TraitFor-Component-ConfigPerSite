1da8e948d29945f3aaea38532804de41345c8384
Aaron J Trevena <aaron.trevena@gmail.com>
2011-08-29 20:23:53 +0100

    over-ride requiredness of schema class at object creation time

--- lib/Catalyst/Model/DBIC/ConfigPerSite.pm
+++ lib/Catalyst/Model/DBIC/ConfigPerSite.pm
@@ -3,11 +3,25 @@ use strict;
 
 use Moose;
 
+use Catalyst::Model::DBIC::Schema::Types
+    qw/ConnectInfo LoadedClass SchemaClass Schema/;
+
+use MooseX::Types::Moose qw/ArrayRef Str ClassName Undef/;
+
+
 extends 'Catalyst::Model::DBIC::Schema'; 
 with qw(Catalyst::TraitFor::Model::DBIC::ConfigPerSite);
 
+has '+schema_class' => (
+    required => 0
+);
+
+
 sub BUILD {
+    warn join ', ', @_;
+    my $self;
     warn "BUILD called\n";
+    $self->SUPER::BUILD();
 }
 
 1;
