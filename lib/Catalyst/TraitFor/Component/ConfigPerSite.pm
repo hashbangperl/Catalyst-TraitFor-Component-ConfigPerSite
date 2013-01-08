d2b6185f9150c825a1a7ed888a4a0a424eabb5c9
Aaron Trevena <aaron.trevena@gmail.com>
2013-01-08 21:04:27 +0000

    bump to version 0.12

--- lib/Catalyst/TraitFor/Component/ConfigPerSite.pm
+++ lib/Catalyst/TraitFor/Component/ConfigPerSite.pm
@@ -61,11 +61,11 @@ Compose this role into your trait to extend a catalyst component such as a model
 
 =head1 VERSION
 
-0.11
+0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 use Moose::Role;
 use MRO::Compat;
